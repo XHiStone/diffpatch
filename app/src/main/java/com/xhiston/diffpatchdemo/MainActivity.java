445da385cbff091a3862cb8413c69f68b9604438
18337129968 <xhf199006232413>
2020-12-14 19:01:13 +0800

    maven-diffpatch

--- app/src/main/java/com/xhiston/diffpatchdemo/MainActivity.java
+++ app/src/main/java/com/xhiston/diffpatchdemo/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+
 import com.xhiston.diffpatch.DiffPatchUtil;
 
 import java.io.File;
